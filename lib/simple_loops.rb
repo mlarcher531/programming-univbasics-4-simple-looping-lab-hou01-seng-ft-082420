2a2da8f83eea8448b69fe1826157ea541bd1ddbc
Mike Archer <mlarcher531@gmail.com>
2020-07-26 15:23:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,19 @@ def loop_message_five_times(message)
     count +=1
   end
   end
+
+def loop_message_n_times(message,limit)
+  count = o
+  while count < limit do
+    puts message
+    count +=1
+  end
+  end
+
+  def output_array(array)
+    count = 0
+    while count < array.length do
+      puts array[count]
+      count+=1
+    end
+  end

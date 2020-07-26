c504d83b5cf8222899ec0ff7510fefb6034f3534
Mike Archer <mlarcher531@gmail.com>
2020-07-26 15:15:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,20 +1,7 @@
 def loop_message_five_times(message)
-  message = "Hello World."
-  puts message
-  puts message
-  puts message
-  puts message
-  puts message
-end
-
-def loop_message_n_times(message, number)
-  message = "Hello Moon."
-  5.times {puts message}
-  number = "Hello Red Balloon."
-  10.times{puts number}
-end
-
-def output_array(array)
-  array = ["Be yourself;", "everyone else is already taken.","- Oscar Wilde"]
-  puts array
-end
+  count = 0
+  while count < 5 do
+    puts message
+    count +=1
+  end
+  end

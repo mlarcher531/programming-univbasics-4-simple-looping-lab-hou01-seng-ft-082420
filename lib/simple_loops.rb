12bd49bac762f4e23e946c98b2c3ea5d8ec4bd97
Mike Archer <mlarcher531@gmail.com>
2020-07-26 15:11:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
-# Write your methods here
+def loop_message_five_times(message)
+  message = "Hello World."
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+def loop_message_n_times(message, number)
+  message = "Hello Moon."
+  5.times {puts message}
+  number = "Hello Red Balloon."
+  10.times{puts number}
+end
+
+def output_array(array)
+  array = ["Be yourself;", "everyone else is already taken.","- Oscar Wilde"]
+  puts array
+end
